9aa6a721d9f5b167d566d7368ecbf3fca138fa0f
klownie <audrikcyeu@proton.me>
2024-10-22 19:59:21 +0200

    fixed togglable name

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,18 @@
 mod templates;
 
-use templates::*;
 use askama_axum::Template;
 use axum::debug_handler;
 use axum::extract::Path;
 use axum::{
-    http::{StatusCode, header, HeaderMap},
+    http::{header, HeaderMap, StatusCode},
     response::{Html, IntoResponse},
     routing::{get, post},
     Json, Router,
 };
 use log::{debug, error, info, trace, warn};
 use std::net::SocketAddr;
+use std::str::FromStr;
+use templates::*;
 use tower_http::services::{ServeDir, ServeFile};
 
 const IP: &'static str = "0.0.0.0";
@@ -19,12 +20,14 @@ const PORT: &'static str = "8080";
 
 #[tokio::main]
 async fn main() {
-    tracing_subscriber::fmt::init();
+    tracing_subscriber::fmt()
+        .with_max_level(tracing::Level::DEBUG)
+        .init();
 
     let app = Router::new()
         .route("/", get(handle_main))
-        .route("/counter/:count", get(handle_counter))  // Added counter route
-        .route("/name/:name", get(toggle_name_handler))  // Added name route
+        .route("/counter/:count", get(handle_counter)) // Added counter route
+        .route("/name/:name", get(toggle_name_handler)) // Added name route
         .nest_service("/assets", ServeDir::new("assets"));
 
     let listen_addr: SocketAddr = format!("{}:{}", IP, PORT).parse().unwrap();
@@ -37,12 +40,9 @@ async fn main() {
 
 #[debug_handler]
 async fn handle_main() -> impl IntoResponse {
-
-
-
     let template = Index {
-        count : 0,
-        name : ToggleName::Normal(String::from("Audrick Yeu")),
+        count: 0,
+        name: ToggleName::Normal(String::from("Audrick Yeu")),
     };
 
     let reply_html = template.render().unwrap();
@@ -51,7 +51,8 @@ async fn handle_main() -> impl IntoResponse {
 
 // Handle /counter/:count route
 #[debug_handler]
-async fn handle_counter(Path(count): Path<u64>) -> impl IntoResponse {
+async fn handle_counter(Path(mut count): Path<u64>) -> impl IntoResponse {
+    count = count + 1;
     let template = TestCounter { count };
     let reply_html = template.render().unwrap();
     (StatusCode::OK, Html(reply_html).into_response())
@@ -59,13 +60,11 @@ async fn handle_counter(Path(count): Path<u64>) -> impl IntoResponse {
 
 // Handle /name/:name route
 #[debug_handler]
-async fn toggle_name_handler(Path(name): Path<ToggleName>) -> impl IntoResponse {
-    // Toggle the name based on the current state
-    let toggled_name = name.toggle();
+async fn toggle_name_handler(Path(name): Path<String>) -> impl IntoResponse {
+    debug!("{:}", name);
+    let toggle_name: ToggleName = ToggleName::from_str(&name).unwrap().toggle(); // Make sure to handle errors
 
-    let template = InteractiveName {
-        name: toggled_name,
-    };
+    let template = InteractiveName { name: toggle_name };
 
     let reply_html = template.render().unwrap();
     (StatusCode::OK, Html(reply_html).into_response())
--- src/templates.rs
+++ src/templates.rs
@@ -7,7 +7,7 @@ use std::str::FromStr;
 #[template(path = "index.html")]
 pub struct Index {
     pub count: u64,
-    pub name:ToggleName,
+    pub name: ToggleName,
 }
 
 #[derive(Template)]
@@ -17,12 +17,10 @@ pub struct TestCounter {
 }
 
 #[derive(Template)]
-#[template(path = "interactive_name.html", print = "ast")]
+#[template(path = "interactive_name.html")]
 pub struct InteractiveName {
     pub name: ToggleName,
 }
-
-
 
 #[derive(Serialize, Deserialize, Clone)]
 pub enum ToggleName {
@@ -34,7 +32,7 @@ impl ToggleName {
     #[inline]
     pub fn toggle(&self) -> Self {
         match self {
-            ToggleName::Normal(_) => ToggleName::AsciArt(String::from("WHAT")),
+            ToggleName::Normal(_) => ToggleName::AsciArt(String::from("WHUTT")),
             ToggleName::AsciArt(_) => ToggleName::Normal(String::from("Audrick Yeu")),
         }
     }
@@ -43,8 +41,31 @@ impl ToggleName {
 impl fmt::Display for ToggleName {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
-            ToggleName::Normal(text) => write!(f, "{}", text),
-            ToggleName::AsciArt(text) => write!(f, "{}", text),
+            ToggleName::Normal(text) => write!(f, "Normal::{}", text),
+            ToggleName::AsciArt(text) => write!(f, "AsciArt::{}", text),
         }
+    }
+}
+
+impl FromStr for ToggleName {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        if s.starts_with("AsciArt::") {
+            Ok(ToggleName::AsciArt(s[9..].to_string()))
+        } else {
+            Ok(ToggleName::Normal(s[8..].to_string()))
+        }
+    }
+}
+
+mod filters {
+    // This filter does not have extra arguments
+    pub fn trimclean<T: std::fmt::Display>(s: T) -> ::askama::Result<String> {
+        let s = s.to_string();
+        let trimed = s
+            .rfind(':')
+            .map_or(None, |pos| Some(s[(pos + 1)..].to_string()));
+        Ok(trimed.expect("didnt find"))
     }
 }
