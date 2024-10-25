a8367c0c0842eb0ba250d70da3b20fa9f6ee25d3
klownie <audrikcyeu@proton.me>
2024-10-25 15:48:04 +0200

    cleaned my code in a cursed way

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,56 @@
 mod templates;
 
+use std::iter::Cycle;
 use askama_axum::Template;
 use axum::debug_handler;
-use axum::extract::Path;
+use axum::extract::{Path, Query, State};
 use axum::{
-    http::{header, HeaderMap, StatusCode},
+    http::StatusCode,
     response::{Html, IntoResponse},
-    routing::{get, post},
-    Json, Router,
+    routing::get,Router,
 };
-use log::{debug, error, info, trace, warn};
+use log::{debug, info};
 use std::net::SocketAddr;
-use std::str::FromStr;
+use std::slice::Iter;
+use std::sync::{Arc, Mutex};
+use text_to_ascii_art::to_art;
 use templates::*;
-use tower_http::services::{ServeDir, ServeFile};
+use tower_http::services::ServeDir;
 
 const IP: &'static str = "0.0.0.0";
 const PORT: &'static str = "8080";
 
+turf::style_sheet!("templates/index.scss");
+
+struct AppState {
+    names: Mutex<Cycle<std::vec::IntoIter<String>>>,
+}
 #[tokio::main]
 async fn main() {
     tracing_subscriber::fmt()
         .with_max_level(tracing::Level::DEBUG)
         .init();
 
+    let app_state = Arc::new(AppState {
+        names: Mutex::new(vec![
+            String::from("Portofolio"),
+            String::from("Universe"),
+            String::from("Audrick Yeu")
+        ].into_iter().cycle()),
+    });
+
+    let api_router = Router::new()
+        .route("/name", get(toggle_name_handler))
+        .with_state(app_state);
+
     let app = Router::new()
+        .nest("/api", api_router)
         .route("/", get(handle_main))
-        .route("/counter/:count", get(handle_counter)) // Added counter route
-        .route("/name/:name", get(toggle_name_handler)) // Added name route
         .nest_service("/assets", ServeDir::new("assets"));
 
     let listen_addr: SocketAddr = format!("{}:{}", IP, PORT).parse().unwrap();
 
-    info!("listening on : {}", listen_addr);
+    info!("listening on : http://localhost:{}", PORT);
 
     let listener = tokio::net::TcpListener::bind(listen_addr).await.unwrap();
     axum::serve(listener, app).await.unwrap();
@@ -40,32 +58,29 @@ async fn main() {
 
 #[debug_handler]
 async fn handle_main() -> impl IntoResponse {
-    let template = Index {
-        count: 0,
-        name: ToggleName::Normal(String::from("Audrick Yeu")),
+
+    let template = Index {};
+    let reply_html = template.render().unwrap();
+    (StatusCode::OK, Html(reply_html).into_response())
+}
+
+
+#[debug_handler]
+async fn toggle_name_handler(
+    State(state): State<Arc<AppState>>,
+) -> impl IntoResponse {
+    // Lock the mutex to get access to the iterator
+    let mut lock = state.names.lock().unwrap();
+
+    // Call `next` to get the next name from the iterator
+    let next_name = lock.next();
+
+    // Format the result depending on whether there's a next value or not
+    let response = match next_name {
+        Some(name) => format!("<pre>{}</pre>", to_art(name,"",0,0,0).unwrap()),
+        None => unreachable!(),
     };
 
-    let reply_html = template.render().unwrap();
-    (StatusCode::OK, Html(reply_html).into_response())
-}
-
-// Handle /counter/:count route
-#[debug_handler]
-async fn handle_counter(Path(mut count): Path<u64>) -> impl IntoResponse {
-    count = count + 1;
-    let template = TestCounter { count };
-    let reply_html = template.render().unwrap();
-    (StatusCode::OK, Html(reply_html).into_response())
-}
-
-// Handle /name/:name route
-#[debug_handler]
-async fn toggle_name_handler(Path(name): Path<String>) -> impl IntoResponse {
-    debug!("{:}", name);
-    let toggle_name: ToggleName = ToggleName::from_str(&name).unwrap().toggle(); // Make sure to handle errors
-
-    let template = InteractiveName { name: toggle_name };
-
-    let reply_html = template.render().unwrap();
-    (StatusCode::OK, Html(reply_html).into_response())
+    // Return the HTML response
+    (StatusCode::OK, Html(response))
 }
--- src/templates.rs
+++ src/templates.rs
@@ -1,71 +1,37 @@
+#[allow(unused)]
+
 use askama_axum::Template;
 use serde::{Deserialize, Serialize};
 use std::fmt;
-use std::str::FromStr;
+use text_to_ascii_art::to_art;
 
+#[derive(Debug, Serialize, Deserialize)]
 #[derive(Template)]
 #[template(path = "index.html")]
-pub struct Index {
-    pub count: u64,
-    pub name: ToggleName,
-}
-
-#[derive(Template)]
-#[template(path = "testcounter.html")]
-pub struct TestCounter {
-    pub count: u64,
-}
-
-#[derive(Template)]
-#[template(path = "interactive_name.html")]
-pub struct InteractiveName {
-    pub name: ToggleName,
-}
-
-#[derive(Serialize, Deserialize, Clone)]
-pub enum ToggleName {
-    Normal(String),
-    AsciArt(String),
-}
-
-impl ToggleName {
-    #[inline]
-    pub fn toggle(&self) -> Self {
-        match self {
-            ToggleName::Normal(_) => ToggleName::AsciArt(String::from("WHUTT")),
-            ToggleName::AsciArt(_) => ToggleName::Normal(String::from("Audrick Yeu")),
-        }
-    }
-}
-
-impl fmt::Display for ToggleName {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match self {
-            ToggleName::Normal(text) => write!(f, "Normal::{}", text),
-            ToggleName::AsciArt(text) => write!(f, "AsciArt::{}", text),
-        }
-    }
-}
-
-impl FromStr for ToggleName {
-    type Err = ();
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        if s.starts_with("AsciArt::") {
-            Ok(ToggleName::AsciArt(s[9..].to_string()))
-        } else {
-            Ok(ToggleName::Normal(s[8..].to_string()))
-        }
-    }
-}
+pub struct Index {}
 
 mod filters {
+    use text_to_ascii_art::to_art;
+
     // This filter does not have extra arguments
-    pub fn trimclean<T: std::fmt::Display>(s: T) -> ::askama::Result<String> {
+    pub fn trimclean<T: std::fmt::Display>(s: T) -> askama::Result<String> {
         let s = s.to_string();
         let trimed = s
-            .rfind(':')
-            .map_or(None, |pos| Some(s[(pos + 1)..].to_string()));
+            .find(':')
+            .map_or(None, |pos| Some(s[(pos + 2)..].to_string()));
         Ok(trimed.expect("didnt find"))
+    }
+
+    pub fn asciart<T: std::fmt::Display>(s: T) -> askama::Result<String> {
+        let s : String = s.to_string();
+        Ok(to_art(s,"",0,0,0).unwrap())
+    }
+
+    pub fn toggle<T: std::fmt::Display>(s: T) -> askama::Result<String> {
+        let s : String = s.to_string();
+        let pos = s.find(':').expect("didnt find the ':'");
+        let front: String = s[..pos.clone()].into();
+        let back:String = s[ (pos + 2).clone()..].into();
+        Ok(format!("{}::{}",back,front))
     }
 }
