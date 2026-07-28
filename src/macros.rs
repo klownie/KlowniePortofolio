afe4566638130d79b80e4ba4f4e37851d23f1aa5
klownie <audrikcyeu@proton.me>
2026-07-28 22:29:27 +0200

    new version with leptos

--- src/macros.rs
+++
@@ -1,9 +0,0 @@
-#[macro_export] macro_rules! uiua {
-    ($code:expr) => {{
-        let mut comp = Compiler::new();
-        let asm = comp.load_str(&format!($code)).unwrap().finish();
-        let mut uiua = Uiua::with_native_sys();
-        uiua.run_asm(asm).unwrap();
-        uiua
-    }}
-}
--- src/main.rs
+++ src/main.rs
@@ -1,104 +1,39 @@
-mod errors;
-mod handlers;
-mod macros;
-mod routes;
-mod templates;
 
-use crate::handlers::Ports;
-use crate::handlers::CONFIG;
-use axum::{
-    handler::HandlerWithoutStateExt,
-    http::{StatusCode, Uri},
-    response::Redirect,
-    BoxError,
-};
-use axum_extra::extract::Host;
-use routes::build_routes;
-use std::net::SocketAddr;
-
-turf::style_sheet!("scss/index.scss");
-
+#[cfg(feature = "ssr")]
 #[tokio::main]
 async fn main() {
-    tracing_subscriber::fmt()
-        .with_max_level(tracing::Level::INFO)
-        .init();
+    use axum::Router;
+    use leptos::logging::log;
+    use leptos::prelude::*;
+    use leptos_axum::{generate_route_list, LeptosRoutes};
+    use portfolio_website::app::*;
 
-    let app = build_routes();
+    let conf = get_configuration(None).unwrap();
+    let addr = conf.leptos_options.site_addr;
+    let leptos_options = conf.leptos_options;
+    // Generate the list of routes in your Leptos App
+    let routes = generate_route_list(App);
 
-    #[cfg(feature = "tls")]
-    {
-        use axum_server::tls_rustls::RustlsConfig;
-        use std::path::PathBuf;
+    let app = Router::new()
+        .leptos_routes(&leptos_options, routes, {
+            let leptos_options = leptos_options.clone();
+            move || shell(leptos_options.clone())
+        })
+        .fallback(leptos_axum::file_and_error_handler(shell))
+        .with_state(leptos_options);
 
-        tokio::spawn(redirect_http_to_https(CONFIG.ports));
-
-        let config = RustlsConfig::from_pem_file(
-            PathBuf::from(env!("CARGO_MANIFEST_DIR"))
-                .join("private_certs")
-                .join("cert.pem"),
-            PathBuf::from(env!("CARGO_MANIFEST_DIR"))
-                .join("private_certs")
-                .join("key.pem"),
-        )
-        .await
-        .expect("failed to load TLS certs");
-
-        let addr = SocketAddr::from(([127, 0, 0, 1], CONFIG.ports.https));
-        tracing::info!("listening on https://{}", addr);
-
-        axum_server::bind_rustls(addr, config)
-            .serve(app.into_make_service_with_connect_info::<SocketAddr>())
-            .await
-            .unwrap();
-    }
-
-    #[cfg(not(feature = "tls"))]
-    {
-        let addr = tokio::net::TcpListener::bind(format!("127.0.0.1:{}", CONFIG.ports.http))
-            .await
-            .unwrap();
-        tracing::info!("listening on http://{}", addr.local_addr().unwrap());
-
-        axum::serve(addr, app).await.unwrap();
-    }
-}
-
-#[allow(dead_code)]
-async fn redirect_http_to_https(ports: Ports) {
-    fn make_https(host: String, uri: Uri, ports: Ports) -> Result<Uri, BoxError> {
-        let mut parts = uri.into_parts();
-
-        parts.scheme = Some(axum::http::uri::Scheme::HTTPS);
-
-        if parts.path_and_query.is_none() {
-            parts.path_and_query = Some("/".parse().unwrap());
-        }
-
-        let https_host = host.replace(&ports.http.to_string(), &ports.https.to_string());
-        parts.authority = Some(https_host.parse()?);
-
-        Ok(Uri::from_parts(parts)?)
-    }
-
-    let redirect = move |Host(host): Host, uri: Uri| async move {
-        match make_https(host, uri, ports) {
-            Ok(uri) => Ok(Redirect::permanent(&uri.to_string())),
-            Err(error) => {
-                tracing::warn!(%error, "failed to convert URI to HTTPS");
-                Err(StatusCode::BAD_REQUEST)
-            }
-        }
-    };
-
-    let addr = tokio::net::TcpListener::bind(format!("127.0.0.1:{}", CONFIG.ports.http))
+    // run our app with hyper
+    // `axum::Server` is a re-export of `hyper::Server`
+    log!("listening on http://{}", &addr);
+    let listener = tokio::net::TcpListener::bind(&addr).await.unwrap();
+    axum::serve(listener, app.into_make_service())
         .await
         .unwrap();
-    tracing::info!("listening on http://{}", addr.local_addr().unwrap());
-    axum::serve(
-        addr,
-        redirect.into_make_service_with_connect_info::<SocketAddr>(),
-    )
-    .await
-    .unwrap();
+}
+
+#[cfg(not(feature = "ssr"))]
+pub fn main() {
+    // no client-side main function
+    // unless we want this to work with e.g., Trunk for pure client-side testing
+    // see lib.rs for hydration function instead
 }
