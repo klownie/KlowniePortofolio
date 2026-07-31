c15f5076bf3fc3ffb822daab7035756f18992cb0
klownie <audrikcyeu@proton.me>
2026-07-31 03:06:01 +0200

    added linux binary

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
   outputs = { self, nixpkgs }: let pkgs = nixpkgs.legacyPackages."x86_64-linux";
   in {
     devShells."x86_64-linux".default = pkgs.mkShell {
-      buildInputs = with pkgs; [cargo rustc cargo-leptos];
+      buildInputs = with pkgs; [cargo rustc cargo-leptos lld wasm-pack dart-sass binaryen nodejs];
     };
   };
 }
