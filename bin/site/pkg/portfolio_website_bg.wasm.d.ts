6735e36c877703b38dd90a82098718e50bcf6baf
klownie <audrikcyeu@proton.me>
2026-07-31 04:12:45 +0200

    needed to add the wasm app

---
+++ bin/site/pkg/portfolio_website_bg.wasm.d.ts
@@ -0,0 +1,38 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const hydrate: () => void;
+export const __wbg_intounderlyingbytesource_free: (a: number, b: number) => void;
+export const __wbg_intounderlyingsink_free: (a: number, b: number) => void;
+export const __wbg_intounderlyingsource_free: (a: number, b: number) => void;
+export const intounderlyingbytesource_autoAllocateChunkSize: (a: number) => number;
+export const intounderlyingbytesource_cancel: (a: number) => void;
+export const intounderlyingbytesource_pull: (a: number, b: any) => any;
+export const intounderlyingbytesource_start: (a: number, b: any) => void;
+export const intounderlyingbytesource_type: (a: number) => number;
+export const intounderlyingsink_abort: (a: number, b: any) => any;
+export const intounderlyingsink_close: (a: number) => any;
+export const intounderlyingsink_write: (a: number, b: any) => any;
+export const intounderlyingsource_cancel: (a: number) => void;
+export const intounderlyingsource_pull: (a: number, b: any) => any;
+export const wasm_bindgen_e9d14e738bfc9899___convert__closures_____invoke___wasm_bindgen_e9d14e738bfc9899___JsValue__core_5acac78f868e195e___result__Result_____wasm_bindgen_e9d14e738bfc9899___JsError___true_: (a: number, b: number, c: any) => [number, number];
+export const wasm_bindgen_e9d14e738bfc9899___convert__closures_____invoke___js_sys_21f0f90eceea3958___Array__web_sys_a535d3c0460dbabd___features__gen_MutationObserver__MutationObserver______true_: (a: number, b: number, c: any, d: any) => void;
+export const wasm_bindgen_e9d14e738bfc9899___convert__closures_____invoke___js_sys_21f0f90eceea3958___Function_fn_wasm_bindgen_e9d14e738bfc9899___JsValue_____wasm_bindgen_e9d14e738bfc9899___sys__Undefined___js_sys_21f0f90eceea3958___Function_fn_wasm_bindgen_e9d14e738bfc9899___JsValue_____wasm_bindgen_e9d14e738bfc9899___sys__Undefined_______true_: (a: number, b: number, c: any, d: any) => void;
+export const wasm_bindgen_e9d14e738bfc9899___convert__closures_____invoke___wasm_bindgen_e9d14e738bfc9899___JsValue______true_: (a: number, b: number, c: any) => void;
+export const wasm_bindgen_e9d14e738bfc9899___convert__closures_____invoke___wasm_bindgen_e9d14e738bfc9899___JsValue______true__1_: (a: number, b: number, c: any) => void;
+export const wasm_bindgen_e9d14e738bfc9899___convert__closures_____invoke___web_sys_a535d3c0460dbabd___features__gen_UiEvent__UiEvent______true_: (a: number, b: number, c: any) => void;
+export const wasm_bindgen_e9d14e738bfc9899___convert__closures_____invoke___web_sys_a535d3c0460dbabd___features__gen_CloseEvent__CloseEvent______true_: (a: number, b: number, c: any) => void;
+export const wasm_bindgen_e9d14e738bfc9899___convert__closures_____invoke___wasm_bindgen_e9d14e738bfc9899___JsValue______true__1__6: (a: number, b: number, c: any) => void;
+export const wasm_bindgen_e9d14e738bfc9899___convert__closures_____invoke___web_sys_a535d3c0460dbabd___features__gen_UiEvent__UiEvent______true__7: (a: number, b: number, c: any) => void;
+export const wasm_bindgen_e9d14e738bfc9899___convert__closures_____invoke___web_sys_a535d3c0460dbabd___features__gen_UiEvent__UiEvent______true__8: (a: number, b: number, c: any) => void;
+export const wasm_bindgen_e9d14e738bfc9899___convert__closures_____invoke___web_sys_a535d3c0460dbabd___features__gen_UiEvent__UiEvent______true__9: (a: number, b: number, c: any) => void;
+export const wasm_bindgen_e9d14e738bfc9899___convert__closures_____invoke_______true_: (a: number, b: number) => void;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_exn_store: (a: number) => void;
+export const __externref_table_alloc: () => number;
+export const __wbindgen_externrefs: WebAssembly.Table;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_destroy_closure: (a: number, b: number) => void;
+export const __externref_table_dealloc: (a: number) => void;
+export const __wbindgen_start: () => void;
